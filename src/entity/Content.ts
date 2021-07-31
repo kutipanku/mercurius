7bae11dc86922883f508b5b48eb3d1acafabc4a1
Maestro Trastanechora <trastanechora@gmail.com>
2021-08-01 04:40:51 +0700

    Create content entity

---
+++ src/entity/Content.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { ObjectType, Field, ID } from 'type-graphql';
+import { Language } from './Language';
+import { Quote } from './Quote';
+
+@ObjectType()
+@Entity('content')
+export class Content extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Field()
+  @Column()
+  text: string;
+
+  @Field()
+  @Column({ name: "quote" })
+  quoteId: string;
+
+  @Field(() => Quote)
+  @ManyToOne(() => Quote, (quote: Quote) => quote.id, { eager : true })
+  @JoinColumn({ name: "quote", referencedColumnName: "id" })
+  quote: Quote;
+
+  @Field()
+  @Column({ name: "language" })
+  languageId: number;
+
+  @Field(() => Language)
+  @ManyToOne(() => Language, (language: Language) => language.id, { eager : true })
+  @JoinColumn({ name: "language", referencedColumnName: "id" })
+  language: Language;
+}
