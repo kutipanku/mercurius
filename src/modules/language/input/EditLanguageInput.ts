8be49cb164755a8aebe65e1af7e52bd5429f4ad8
Maestro Trastanechora <maestro.trastanechora@BA-00018657.local>
2021-07-31 11:57:24 +0700

    Add language input

---
+++ src/modules/language/input/EditLanguageInput.ts
@@ -0,0 +1,12 @@
+import { Length } from 'class-validator';
+import { Field, InputType } from 'type-graphql';
+
+@InputType()
+export class EditLanguageInput {
+  @Field()
+  id: number;
+
+  @Field()
+  @Length(1, 255)
+  name: string;
+}
